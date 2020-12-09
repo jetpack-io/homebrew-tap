dd7c658c63c42c9f430c7cf4fd7d92d78b766608
Daniel Loreto <279789+loreto@users.noreply.github.com>
2020-12-08 18:01:16 -0800

    Add sha

--- Formula/jetpack.rb
+++ Formula/jetpack.rb
@@ -2,13 +2,11 @@ class Foo < Formula
   desc "Jetpack"
   homepage "https://www.jetpack.io"
 
-  # For now download releases directly from GitHub, but eventually we should host
-  # them at releases.jetpack.io:
-  # url "https://releases.jetpack.io/jetpack/v0.1.0/jetpack_0.1.0_darwin_amd64.zip"
-  url "https://github.com/jetpack-io/jetpack/releases/download/v0.1.0/jetpack_0.1.0_darwin_amd64.zip"
+  url "https://releases.jetpack.io/jetpack/0.1.0/jetpack_0.1.0-alpha_darwin_amd64.zip"
 
-  sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
-  version "0.1.0"
+  sha256 "0a2358a0cbf1965101b73404a7a93ff8a2da084dec6f64d5e74f64aa2ce51754"
+  # Version numbers should follow the semantic versioning spec
+  version "0.1.0-alpha"
   bottle :unneeded
 
   def install

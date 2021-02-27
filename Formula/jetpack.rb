98f01f1626e20c957ed7fc18f798f82221039204
Daniel Loreto <279789+loreto@users.noreply.github.com>
2021-02-26 17:09:29 -0800

    Update to a tar file

--- Formula/jetpack.rb
+++ Formula/jetpack.rb
@@ -2,7 +2,7 @@ class Jetpack < Formula
   desc "Jetpack"
   homepage "https://www.jetpack.io"
 
-  url "https://releases.jetpack.io/jetpack/0.1.0-preview.2021.02.26/jetpack_0.1.0-preview.2021.02.26_darwin_amd64.zip"
+  url "https://releases.jetpack.io/jetpack/0.1.0-preview.2021.02.26/jetpack_0.1.0-preview.2021.02.26_darwin_amd64.tar.gz"
 
   sha256 "cb9a2fbc6a4de9c8a78404ff7ade1c7c404538b6d0535f4e6f325dc3bf97987e"
   # Version numbers should follow the semantic versioning spec
